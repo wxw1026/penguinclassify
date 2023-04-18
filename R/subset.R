55d4f7eb0e6ac3df4bc11e9c3ce2335d318ff7f5
wxw1026 <xiwen1026@yeah.net>
2023-04-17 20:57:26 -0700

    mm

--- R/subset.R
+++ R/subset.R
@@ -1,7 +1,18 @@
 #' return the species column for palmerpenguins
+#' @importFrom rlang .data
+#'
 #' @return dataframe of one column
+#'
 #' @export
+#'
+#' @examples
+#' get_species()
+#'
+#' \dontrun{
+#' get_species("hi")
+#' }
+#'
 get_species <- function() {
   palmerpenguins::penguins %>%
-    dplyr::select(species)
+    dplyr::select(.data$species)
 }
